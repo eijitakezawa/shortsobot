2a4d4e9bd3b119312bb9db6deda205f89915c907
eijitakezawa@gmail.com <eijitakezawa@gmail.com>
2025-08-06 17:16:41 +0900

    fix: Flask起動処理を追加

--- app.py
+++ app.py
@@ -47,3 +47,6 @@ def handle_message(event):
             messages=[TextMessage(text=reply)]
         )
     )
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
